54d21f86544c798f0ce34cd95449aafd7d144383
Aggronovec <juraj.sedlak07@gmail.com>
2023-06-26 17:10:24 +0200

    Monke v0.1

--- src/main/java/net/monke/abrakadavra/Abrakadavra.java
+++ src/main/java/net/monke/abrakadavra/Abrakadavra.java
@@ -1,4 +1,4 @@
-package net.aggrondavid.sorceryverse;
+package net.monke.abrakadavra;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,14 +18,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(SorceryVerse.MOD_ID)
-public class SorceryVerse
+@Mod(Abrakadavra.MOD_ID)
+public class Abrakadavra
 {
-    public static final String MOD_ID = "sorceryverse";
+    public static final String MOD_ID = "abrakadavra";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public SorceryVerse() {
+    public Abrakadavra() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
